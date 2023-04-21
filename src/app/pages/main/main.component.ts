73fbaf745e0aefdfef064b43428bf100bfa7adb7
Alvaro GR <alvarog_r@tutanota.com>
2023-04-21 09:16:32 +0200

    Agregada descripcion de secciones a main-page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface IMainPageData {
+  title: string;
+  route: string;
+  content?: string;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,13 +13,39 @@ import { Component } from '@angular/core';
 })
 
 export class MainComponent {
-  navigationData = [
-    { title: 'Creación de observables', route: '/creacion-observables' },
-    { title: 'Operadores básicos', route: '/operadores-basicos' },
-    { title: 'Utilidades', route: '/utilidades' },
-    { title: 'Operadores temporales', route: '/operadores-temporales' },
-    { title: 'Combinación de observables', route: '/combinacion-observables' },
-    { title: 'High Order Observables', route: '/high-order-observables' },
-    { title: 'Utilidades avanzadas', route: '/utilidades-avanzadas' },
+  mainPageData: Array<IMainPageData> = [
+    { 
+      title: 'Creación de observables', 
+      route: '/creacion-observables',
+      content: 'Create, from, range, of, interval, time, fromEvent'
+    },
+    { title: 'Operadores básicos', 
+      route: '/operadores-basicos', 
+      content: 'mapTo, map, filter, tap, first, take, takeWhile, last, takeLast, skip, reduce, scan' 
+    },
+    { title: 'Utilidades', 
+      route: '/utilidades', 
+      content: 'startWith, endWith, distinct, distinctUntilChanged, pairwise, share, Subject, BehaviorSubject, Hot Observables' 
+    },
+    { title: 'Operadores temporales', 
+      route: '/operadores-temporales', 
+      content: 'sampleTime, throttleTime, auditTime, delay, bufferTime, debounceTime' 
+    },
+    { title: 'Combinación de observables', 
+      route: '/combinacion-observables', 
+      content: 'zip, merge, concat, forkJoin, combineLatest, withLatestFrom' 
+    },
+    { title: 'High Order Observables', 
+      route: '/high-order-observables', 
+      content: 'mergeAll, mergeMap, switchMap, concatMap' 
+    },
+    { title: 'Utilidades avanzadas', 
+      route: '/utilidades-avanzadas', 
+      content: 'throwError, catchError, retry, NEVER, EMPTY, materialize, dematerialize' 
+    },
+    { title: 'Tic Tac Toe', 
+      route: '/tic-tac-toe', 
+      content: '' 
+    },
   ]
 }
